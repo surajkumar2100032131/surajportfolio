f5b798913062b0ceb2b9e99e3b6ce9772dd72348
surajkumar2100032131 <110281881+surajkumar2100032131@users.noreply.github.com>
2025-12-08 02:29:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
 
 //home
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Web development" , "Customer communication" , "Relationship building"],
+    strings: ["Programming" , "Web development" , "Interaction" , "Relationship building"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
@@ -253,5 +253,6 @@ const canvas = document.getElementById('sparkCanvas');
 
         updateCanvasSize();
         animateParticles();
+
 
 
